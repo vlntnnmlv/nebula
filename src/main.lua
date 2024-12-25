f1c086b9d14724231533c4acce6bc4d2d32fe595
valentinnamleev <vlntnnmlv@gmail.com>
2024-12-25 18:19:33 +0100

    Add parent/child system & text nodes

--- src/main.lua
+++ src/main.lua
@@ -1,10 +1,12 @@
 dofile("src/node.lua")
 
 function love.load()
-    Node.new(0, 0, 1080, 720, { r = 1.0, g = 1.0, b = 1.0, a = 1.0})
+    root = Node.new(nil, 0, 0, 1080, 720, Color(1.0, 1.0, 1.0, 1.0))
+    fps = Node.text(root, 0, 50, 15, 16, Color(0.0, 1.0, 0.0, 1.0))
 
-    Node.new(456, 44, 400, 300, nil)
-    Node.new(300, 60, 300, 260, nil)
+    n1 = Node.new(root, 456, 44, 400, 300, nil)
+    Node.new(root, 300, 60, 300, 260, nil)
+    Node.text(n1, "Play", 456, 88, 32, Color(0.0, 0.0, 1.0, 1.0) )
 end
 
 function love.draw()
@@ -12,6 +14,7 @@ function love.draw()
 end
 
 function love.update(dt)
+    fps.text = love.timer.getFPS()
 end
 
 function love.mousepressed(x, y, button)
--- src/node.lua
+++ src/node.lua
@@ -1,17 +1,40 @@
+function Color(r,g,b,a)
+    return { r = r, g = g, b = b, a = a}
+end
+
 Node = {}
 
 Node.nodes = {}
-Node.next_id = 1
+Node.nodesCount = 0
 
 local function register_node(node)
-    node.id = Node.next_id
-    Node.next_id = Node.next_id + 1
+    node.id = Node.nodesCount
+    Node.nodesCount = Node.nodesCount + 1
 
     Node.nodes[node.id] = node
 end
 
-Node.new = function(x, y, w, h, color)
+Node.new = function(parent, x, y, w, h, color)
     local self = {}
+    
+    self.parent = parent
+    self.children = {}
+    self.childrenCount = 0
+
+    self.linkChild = function(child)
+        self.children[self.childrenCount] = child
+        self.childrenCount = self.childrenCount + 1
+    end
+
+    self.getParentID = function()
+        if self.parent == nil then return "nil" end
+
+        return self.parent.id
+    end
+
+    if self.parent ~= nil then
+        self.parent.linkChild(self)
+    end
 
     self.x = x
     self.y = y
@@ -19,9 +42,15 @@ Node.new = function(x, y, w, h, color)
     self.h = h
 
     if color == nil then
-        self.color = { r = 0.0, g = 0.0, b = 0.0, a = 0.1 } 
+        self.color = Color(0.0, 0.0, 0.0, 0.1)
     else
         self.color = color
+    end
+
+    self.drawChildren = function()
+        for i = 0, self.childrenCount - 1 do
+            self.children[i].draw()
+        end
     end
 
     self.draw = function()
@@ -41,7 +70,8 @@ Node.new = function(x, y, w, h, color)
             self.x + self.w, self.y + self.h,
             self.x, self.y + self.h
         )
-        love.graphics.print(self.id, self.x + self.w / 2, self.y + self.h / 2)
+
+        self.drawChildren()
     end
 
     register_node(self)
@@ -49,8 +79,26 @@ Node.new = function(x, y, w, h, color)
     return self
 end
 
-Node.drawAll = function()
-    for i = 1, Node.next_id - 1 do
-        Node.nodes[i].draw()
+Node.text = function(parent, text, x, y, fontSize, color)
+    local self = Node.new(parent, x, y, 0, 0, color)
+
+    self.text = text
+    self.font = love.graphics.newFont(fontSize)
+    self.w = self.font:getWidth(self.text)
+    self.h = self.font:getBaseline(self.text)
+
+    self.draw = function()
+        love.graphics.setFont(self.font)
+
+        love.graphics.setColor(self.color.r, self.color.g, self.color.b, self.color.a)
+        love.graphics.print(self.text, self.x - self.w / 2, self.y - self.h / 2)
     end
+
+    return self
+end
+
+Node.drawAll = function()
+    if Node.nodesCount == 0 then return end
+
+    Node.nodes[0].draw()
 end
